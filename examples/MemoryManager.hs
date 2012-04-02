9e3f82933b11dd24a071db464dd7ed007657a6eb
Christiaan Baaij <christiaan.baaij@gmail.com>
2012-04-02 16:43:49 +0200

    Update MemoryManager to behave like lutz's C-sharp code

--- examples/MemoryManager.hs
+++ examples/MemoryManager.hs
@@ -6,38 +6,38 @@ import SoOSiM
 import MemoryManager.Types
 import MemoryManager.Util
 
-data MemState =
-  MemState { localAddress  :: [Int]
-           , remoteAddress :: IntMap ComponentId
-           }
-
 memoryManager :: MemState -> ComponentInput -> SimM MemState
-memoryManager s (ComponentMsg senderId msgContent) = do
-  let addrMaybe = identifyAddress msgContent
-  case addrMaybe of
-    Just addr ->
-      case (addr `elem` localAddress s) of
-        True  ->
-          case (memCommand msgContent) of
-            Read _  -> do
-              addrVal <- readMemory Nothing addr
-              invokeNoWait Nothing senderId addrVal
-              return s
-            Write _ val -> do
-              writeMemory Nothing addr (toDyn val)
-              invokeNoWait Nothing senderId (toDyn True)
-              return s
-        False -> do
-          creator <- componentCreator
-          let remote = findWithDefault creator addr (remoteAddress s)
-          response <- invoke Nothing remote msgContent
-          invokeNoWait Nothing senderId response
-          return s
-    Nothing -> return s
+memoryManager s (ComponentMsg senderId msgContent)
+  | Just (Register addr sc src) <- fromDynamic msgContent
+  = return $ s {addressLookup = (MemorySource addr sc src):(addressLookup s)}
+
+  | Just (Read addr) <- fromDynamic msgContent
+  = do
+    let src = checkAddress (addressLookup s) addr
+    case (sourceId src) of
+      Nothing -> do
+        addrVal <- readMemory Nothing addr
+        invokeNoWait Nothing senderId addrVal
+        return s
+      Just remote -> do
+        response <- invoke Nothing remote msgContent
+        invokeNoWait Nothing senderId response
+        return s
+
+  | Just (Write addr val) <- fromDynamic msgContent
+  = do
+    let src = checkAddress (addressLookup s) addr
+    case (sourceId src) of
+      Nothing -> do
+        addrVal <- writeMemory Nothing addr val
+        return s
+      Just remote -> do
+        invokeNoWait Nothing remote msgContent
+        return s
 
 memoryManager s _ = return s
 
 instance ComponentIface MemState where
-  initState          = MemState [] empty
+  initState          = MemState []
   componentName _    = "MemoryManager"
   componentBehaviour = memoryManager
