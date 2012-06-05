0ae81f5e0f3a3e1e248c2e392c1d5f93c86de8e1
Christiaan Baaij <christiaan.baaij@gmail.com>
2012-06-05 17:04:53 +0200

    Update SoOSiM module

--- src/SoOSiM.hs
+++ src/SoOSiM.hs
@@ -1,41 +1,22 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 module SoOSiM
-  ( SimM
+  ( Sim
   , ComponentId
   , NodeId
-  , ComponentIface (..)
-  , ComponentInput (..)
+  , ComponentInterface (..)
+  , Input (..)
   , module SoOSiM.SimMonad
-  , Dynamic
-  , Typeable
-  , marshall
-  , safeUnmarshall
   , unmarshall
   , ignore
   )
 where
 
-import Data.Dynamic
-
 import SoOSiM.SimMonad
-import SoOSiM.Types
+import SoOSiM.Types    (Sim,ComponentId,NodeId,ComponentInterface(..)
+                       ,Input(..))
+import SoOSiM.Util     (unmarshall)
 
-marshall :: Typeable a => a -> Dynamic
-marshall = toDyn
-
-safeUnmarshall :: Typeable a => Dynamic -> Maybe a
-safeUnmarshall = fromDynamic
-
-unmarshall :: forall a . Typeable a => Dynamic -> a
-unmarshall d = fromDyn d
-                 (error $  "unmarshal failed: expected value of type: "
-                        ++ show resDyn
-                        ++ ", received value of type: "
-                        ++ show d
-                 )
-  where
-    resDyn :: Dynamic
-    resDyn = toDyn (undefined :: a)
-
-ignore :: Dynamic -> SimM ()
+ignore ::
+  ComponentInterface s
+  => (Send s)
+  -> Sim ()
 ignore = const (return ())
